6367740a2d5de1eaf99fc171065ef069e1e41006
chlorwu <chloewu3439@gmail.com>
2026-07-14 19:34:16 -0700

    firebase.js added for website

---
+++ website/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuAeN0FrNvAHx0_rMP9F9JrJjiiFC_dO4",
+  authDomain: "landbot-92fa9.firebaseapp.com",
+  databaseURL: "https://landbot-92fa9-default-rtdb.firebaseio.com",
+  projectId: "landbot-92fa9",
+  storageBucket: "landbot-92fa9.firebasestorage.app",
+  messagingSenderId: "216362459559",
+  appId: "1:216362459559:web:937f69b81161ab5035d682"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
